17b86f66e0f9c839a1122335da34313286d0c32a
🧑🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2023-02-07 14:50:49 +0100

    chore: set up pull request template

--- .projenrc.ts
+++ .projenrc.ts
@@ -14,6 +14,16 @@ const project = new typescript.TypeScriptProject({
   homepage: 'https://aws.github.io/jsii',
   repository: 'https://github.com/aws/jsii-compiler.git',
 
+  pullRequestTemplateContents: [
+    '',
+    '',
+    '---',
+    '',
+    'By submitting this pull request, I confirm that my contribution is made under the terms of the [Apache 2.0 license].',
+    '',
+    '[Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0',
+  ],
+
   autoDetectBin: true,
 
   minNodeVersion: '14.18.0',
