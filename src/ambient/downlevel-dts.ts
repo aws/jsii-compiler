c82b1815fbbd92374e0a0c0603f78522217a41ec
🧑🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2023-03-01 14:20:43 +0100

    fix: do not pass semver objects to downlevel-dts
    If `downlevel-dts` has a different `semver` dependency instance than the
    jsii compiler, an `instanceof` check internal to `semver` fails and
    produces a `TypeError` exception. We can easily avoid this by always
    providing the target version as a `string`, which leads to
    `downlevel-dts` ultimately using its own `semver` instance to turn it
    into the `semver.SemVer` object it needs.

--- src/ambient/downlevel-dts.ts
+++ src/ambient/downlevel-dts.ts
@@ -1,7 +1,5 @@
 /** Hand-written declaration for the downlevel-dts module */
 declare module 'downlevel-dts' {
-  import { SemVer } from 'semver';
-
   /**
    * Rewrite .d.ts files created by any version of TypeScript so that they work
    * with TypeScript 3.4 or later. It does this by converting code with new
@@ -10,6 +8,12 @@ declare module 'downlevel-dts' {
    * @param src           the directory containing the original .d.ts files
    * @param target        the directory in which to place re-written files
    * @param targetVersion the target TypeScript version compatibility
+   *
+   * @note The "real" signature would allow semver.SemVer instances to be
+   *       provided as `targetVersion`, but some code-path involves an
+   *       `instanceof` test which will fail if somehow `downlevel-dts` is
+   *       provided with its own install of the `semver` dependency, which we
+   *       cannot control, so we disallow using `semver.SemVer` instances here.
    */
-  export function main(src: string, target: string, targetVersion: string | SemVer): void;
+  export function main(src: string, target: string, targetVersion: string): void;
 }
