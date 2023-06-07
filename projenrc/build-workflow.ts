259a8aa3c47ee467b0dfb2f04216919121aaea1f
Naumel <naume@amazon.com>
2023-06-07 10:20:30 +0200

    Revert "chore: Triggering merge group checks"
    This reverts commit 9d33255d47ab8660caff016d7a4084d9add9631d.

--- projenrc/build-workflow.ts
+++ projenrc/build-workflow.ts
@@ -376,9 +376,6 @@ export class BuildWorkflow {
         pullRequestTarget: {
           types: ['opened', 'reopened', 'ready_for_review'],
         },
-        mergeGroup: {
-          branches: ['main'],
-        },
       });
       autoMerge.addJob('enable-auto-merge', {
         env: { CI: 'true' },
