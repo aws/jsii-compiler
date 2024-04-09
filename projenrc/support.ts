b7dc577540c461bce1b3c090869892b66ccbe928
AWS CDK Automation <43080478+aws-cdk-automation@users.noreply.github.com>
2024-04-08 17:27:58 -0700

    chore(deps): upgrade dependencies (#860)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]:
    https://github.com/aws/jsii-compiler/actions/runs/8608334974
    
    ------
    
    *Automatically created by projen via the "upgrade-maintenance-v5.2"
    workflow*
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- projenrc/support.ts
+++ projenrc/support.ts
@@ -2,12 +2,13 @@ import { JsonFile, Project } from 'projen';
 import type { ReleasesDocument } from '../src/support';
 
 export const SUPPORT_POLICY: ReleasesDocument = {
-  current: '5.3',
+  current: '5.4',
   maintenance: {
     // version: End-of-support date
     '5.0': new Date('2024-01-31'),
     '5.1': new Date('2024-02-28'),
     '5.2': new Date('2024-06-30'),
+    '5.3': new Date('2024-10-15'),
   },
 };
 
