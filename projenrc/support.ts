acd3d84d76eb8c51f57110163bc1a437d46793cc
Momo Kornher <kornherm@amazon.co.uk>
2024-05-16 10:33:12 +0100

    chore: enable backports of PRs to previous version lines (#976)
    Because this project is using a custom release branches component, we
    have to instantiate the `PullRequestBackport` component manually (as
    opposed to project options).
    
    Includes a couple of refactors to make working with `SupportPolicy`
    easier, these are copied from jsii-rosetta.
    
    ---
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the [Apache 2.0 license].
    
    [Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0

--- projenrc/support.ts
+++ projenrc/support.ts
@@ -13,6 +13,20 @@ export const SUPPORT_POLICY: ReleasesDocument = {
 };
 
 export class SupportPolicy {
+  public get branches(): {
+    [version: string]: string;
+  } {
+    const branches = {
+      [SUPPORT_POLICY.current]: 'main',
+    };
+
+    for (const version of Object.keys(SUPPORT_POLICY.maintenance)) {
+      branches[version] = `maintenance/v${version}`;
+    }
+
+    return branches;
+  }
+
   public constructor(project: Project) {
     new JsonFile(project, 'releases.json', {
       allowComments: false,
@@ -20,5 +34,23 @@ export class SupportPolicy {
       obj: SUPPORT_POLICY,
       readonly: true,
     });
+  }
+
+  /**
+   * Get all actively maintained branches
+   */
+  public activeBranches(includeCurrent = true): {
+    [version: string]: string;
+  } {
+    return Object.fromEntries(
+      Object.entries(this.branches).filter(([version]) => {
+        if (includeCurrent && version === SUPPORT_POLICY.current) {
+          return true;
+        }
+
+        // check if branch is still maintained
+        return Date.now() <= SUPPORT_POLICY.maintenance[version as any]?.getTime();
+      }),
+    );
   }
 }
