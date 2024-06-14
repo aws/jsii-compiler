8501121c20027d6440196123cabedf5512b2d6dc
Sumu <sumughan@amazon.com>
2024-06-14 14:41:48 -0400

    fix run script
    Signed-off-by: Sumu <sumughan@amazon.com>

--- projenrc/benchmark-test.ts
+++ projenrc/benchmark-test.ts
@@ -154,8 +154,7 @@ export class BenchmarkTest {
           {
             name: 'Publish Metrics',
             //if: `github.event.repository.fork == false && github.ref == 'refs/heads/main'`,
-            run: `
-            |-
+            run: `|-
             aws cloudwatch put-metric-data --namespace JsiiPerformance --metric-data '[
               {
                 "MetricName": "TSC-average",
