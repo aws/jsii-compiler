631ffe0f894cc1d3c37337fe861733342a06c1c4
mikewrighton <mike.wrighton@googlemail.com>
2024-08-19 14:23:52 -0400

    Upgrade TypeScript to 5.5.x.

--- projenrc/support.ts
+++ projenrc/support.ts
@@ -2,13 +2,14 @@ import { JsonFile, Project } from 'projen';
 import type { ReleasesDocument } from '../src/support';
 
 export const SUPPORT_POLICY: ReleasesDocument = {
-  current: '5.4',
+  current: '5.5',
   maintenance: {
     // version: End-of-support date
     '5.0': new Date('2024-01-31'),
     '5.1': new Date('2024-02-28'),
     '5.2': new Date('2024-06-30'),
     '5.3': new Date('2024-10-15'),
+    '5.4': new Date('2024-02-19'),
   },
 };
 
