aws/jsii-compiler

commit 27873df20365b0c1651adb8d969fcf69ce5e4267

fix: deprecation warnings source file too large (#2620)
The current deprecation warnings generator generates a validation
function for every type, even if it doesn't contain any deprecated
members or doesn't reference any other types that contain deprecated
members.

The upshot is that the `.warnings.jsii.js` file is 5.8MB of this:

```ts
function aws_cdk_lib_alexa_ask_CfnSkill(p) {
}
function aws_cdk_lib_alexa_ask_CfnSkill_AuthenticationConfigurationProperty(p) {
}
function aws_cdk_lib_alexa_ask_CfnSkill_SkillPackageProperty(p) {
    if (p == null)
        return;
    visitedObjects.add(p);
    try {
        if (!visitedObjects.has(p.overrides))
            aws_cdk_lib_IResolvable(p.overrides);  // <-- all this does is a function call that doesn't do anything either
    }
    finally {
        visitedObjects.delete(p);
    }
}
function aws_cdk_lib_IResolvable(p) {
}
```

Which is rather wasteful.

Main change
-----------

In this PR, we only generate validation functions for types that
actually contain deprecated members, or types that transitively
reference types that contain deprecated members.

To keep the API the same (for potential external code calling into this
library for external validation, but see below), we play the following
trick:

```ts
module.exports = new Proxy({}, {
  get: function(_, prop) {
    return actualValidationFunction(prop) ?? noOpFunction();
  }
});
```

We replace the module with a `Proxy`, so that every validation function
that is requested exists. Only for those functions we actually render do
we return an actual validation function, otherwise we return a singleton
no-op function.

This brings the size of `.warnings.jsii.js` down to ~400K, saving ~5.5MB
of functions that don't do anything. Plus, it *slightly* reduces the
cost of run-time checking structures that don't contain any deprecated
properties.

Cross-assembly type references
------------------------------

What do we do with the following type:

```ts

fixtures/@scope/jsii-calc-base/lib/index.ts
@@ -19,6 +19,10 @@ export abstract class Base {
   }
 }
 
+export interface TypeToContainVeryBaseProps {
+  readonly veryBaseProps: VeryBaseProps;
+}
+
 export interface BaseProps extends VeryBaseProps {
   readonly bar: string;
 }

test/deprecation-warnings.test.ts
@@ -15,7 +15,7 @@ describe('Function generation', () => {
       addDeprecationWarnings: true,
     });
 
-    expect(jsFile(result, '.warnings.jsii')).toBe(
+    expect(jsFile(result, '.warnings.jsii')).toContain(
       `function print(name, deprecationMessage) {
     const deprecated = process.env.JSII_DEPRECATED;
     const deprecationMode = ["warn", "fail", "quiet"].includes(deprecated) ? deprecated : "warn";
@@ -52,15 +52,19 @@ class DeprecationError extends Error {
         });
     }
 }
-module.exports = { print, getPropertyDescriptor, DeprecationError };
 `,
     );
   });
 
-  test('generates a function for each type', () => {
+  test('generates validation functions only for types with deprecated members', () => {
     const result = compileJsiiForTest(
       `
-        export interface Foo {}
+        export interface Foo {
+          /**
+           * @deprecated Do not use
+           */
+          readonly foo: string;
+        }
         export interface Bar {}
         export interface Baz {}
         `,
@@ -68,14 +72,9 @@ module.exports = { print, getPropertyDescriptor, DeprecationError };
       { addDeprecationWarnings: true },
     );
 
-    expect(jsFile(result, '.warnings.jsii')).toMatch(
-      `function testpkg_Foo(p) {
-}
-function testpkg_Bar(p) {
-}
-function testpkg_Baz(p) {
-}`,
-    );
+    expect(jsFile(result, '.warnings.jsii')).toContain('testpkg_Foo(');
+    expect(jsFile(result, '.warnings.jsii')).not.toContain('testpkg_Bar(');
+    expect(jsFile(result, '.warnings.jsii')).not.toContain('testpkg_Baz(');
   });
 
   test('generates metadata', () => {
@@ -91,10 +90,15 @@ function testpkg_Baz(p) {
     expect(result.assembly.metadata?.jsii?.compiledWithDeprecationWarnings).toBe(true);
   });
 
-  test('for each non-primitive property, generates a call', () => {
+  test('generates a call for calling into objects with deprecated members', () => {
     const result = compileJsiiForTest(
       `
-        export interface Foo {}
+        export interface Foo {
+          /**
+           * @deprecated Do not use
+           */
+          readonly foo: string;
+        }
         export interface Bar {}
         export interface Baz {
           readonly foo: Foo;
@@ -106,23 +110,23 @@ function testpkg_Baz(p) {
       { addDeprecationWarnings: true },
     );
 
-    expect(jsFile(result, '.warnings.jsii')).toMatch(`function testpkg_Baz(p) {
-    if (p == null)
-        return;
-    visitedObjects.add(p);
-    try {
-        if (!visitedObjects.has(p.bar))
-            testpkg_Bar(p.bar);
-        if (!visitedObjects.has(p.foo))
-            testpkg_Foo(p.foo);
-    }
-    finally {
-        visitedObjects.delete(p);
-    }
-}`);
+    expect(jsFunction(result, 'testpkg_Baz', '.warnings.jsii')).toMatchInlineSnapshot(`
+      "function testpkg_Baz(p) {
+              if (p == null)
+                  return;
+              visitedObjects.add(p);
+              try {
+                  if (!visitedObjects.has(p.foo))
+                      module.exports.testpkg_Foo(p.foo);
+              }
+              finally {
+                  visitedObjects.delete(p);
+              }
+          } "
+    `);
   });
 
-  test('generates empty functions for interfaces', () => {
+  test('generates no functions for interfaces', () => {
     const result = compileJsiiForTest(
       `
         export interface IFoo {
@@ -133,11 +137,10 @@ function testpkg_Baz(p) {
       { addDeprecationWarnings: true },
     );
 
-    expect(jsFile(result, '.warnings.jsii')).toMatch(`function testpkg_IFoo(p) {
-}`);
+    expect(jsFile(result, '.warnings.jsii')).not.toContain('testpkg_IFoo(');
   });
 
-  test('generates empty functions for classes', () => {
+  test('generates no functions for classes', () => {
     const result = compileJsiiForTest(
       `
         export class Foo {
@@ -148,8 +151,7 @@ function testpkg_Baz(p) {
       { addDeprecationWarnings: true },
     );
 
-    expect(jsFile(result, '.warnings.jsii')).toMatch(`function testpkg_Foo(p) {
-}`);
+    expect(jsFile(result, '.warnings.jsii')).not.toContain(`testpkg_Foo(`);
   });
 
   test('generates calls for recursive types', () => {
@@ -161,26 +163,31 @@ function testpkg_Baz(p) {
       { addDeprecationWarnings: true },
     );
 
-    expect(jsFile(result, '.warnings.jsii')).toMatch(
-      `function testpkg_Bar(p) {
-    if (p == null)
-        return;
-    visitedObjects.add(p);
-    try {
-        if (!visitedObjects.has(p.bar))
-            testpkg_Bar(p.bar);
-    }
-    finally {
-        visitedObjects.delete(p);
-    }
-}`,
-    );
+    expect(jsFunction(result, 'testpkg_Bar', '.warnings.jsii')).toMatchInlineSnapshot(`
+      "function testpkg_Bar(p) {
+              if (p == null)
+                  return;
+              visitedObjects.add(p);
+              try {
+                  if (!visitedObjects.has(p.bar))
+                      module.exports.testpkg_Bar(p.bar);
+              }
+              finally {
+                  visitedObjects.delete(p);
+              }
+          } "
+    `);
   });
 
   test('checks array elements', () => {
     const result = compileJsiiForTest(
       `
-      export interface Used { readonly property: boolean; }
+      export interface Used {
+        /**
+         * @deprecated Oh no
+         */
+        readonly property: boolean;
+      }
       export interface Uses { readonly array: Used[]; }
       `,
       undefined /* callback */,
@@ -189,26 +196,31 @@ function testpkg_Baz(p) {
 
     expect(jsFunction(result, 'testpkg_Uses', '.warnings.jsii')).toMatchInlineSnapshot(`
       "function testpkg_Uses(p) {
-          if (p == null)
-              return;
-          visitedObjects.add(p);
-          try {
-              if (p.array != null)
-                  for (const o of p.array)
-                      if (!visitedObjects.has(o))
-                          testpkg_Used(o);
-          }
-          finally {
-              visitedObjects.delete(p);
-          }
-      }"
+              if (p == null)
+                  return;
+              visitedObjects.add(p);
+              try {
+                  if (p.array != null)
+                      for (const o of p.array)
+                          if (!visitedObjects.has(o))
+                              module.exports.testpkg_Used(o);
+              }
+              finally {
+                  visitedObjects.delete(p);
+              }
+          } "
     `);
   });
 
   test('checks map elements', () => {
     const result = compileJsiiForTest(
       `
-      export interface Used { readonly property: boolean; }
+      export interface Used {
+        /**
+         * @deprecated Oh no
+         */
+        readonly property: boolean;
+      }
       export interface Uses { readonly map: Record<string, Used>; }
       `,
       undefined /* callback */,
@@ -217,36 +229,20 @@ function testpkg_Baz(p) {
 
     expect(jsFunction(result, 'testpkg_Uses', '.warnings.jsii')).toMatchInlineSnapshot(`
       "function testpkg_Uses(p) {
-          if (p == null)
-              return;
-          visitedObjects.add(p);
-          try {
-              if (p.map != null)
-                  for (const o of Object.values(p.map))
-                      if (!visitedObjects.has(o))
-                          testpkg_Used(o);
-          }
-          finally {
-              visitedObjects.delete(p);
-          }
-      }"
+              if (p == null)
+                  return;
+              visitedObjects.add(p);
+              try {
+                  if (p.map != null)
+                      for (const o of Object.values(p.map))
+                          if (!visitedObjects.has(o))
+                              module.exports.testpkg_Used(o);
+              }
+              finally {
+                  visitedObjects.delete(p);
+              }
+          } "
     `);
-  });
-
-  test('generates exports for all the functions', () => {
-    const result = compileJsiiForTest(
-      `
-        export interface Foo {}
-        export interface Bar {}
-        export interface Baz {}
-        `,
-      undefined /* callback */,
-      { addDeprecationWarnings: true },
-    );
-
-    expect(jsFile(result, '.warnings.jsii')).toMatch(
-      'module.exports = { print, getPropertyDescriptor, DeprecationError, testpkg_Foo, testpkg_Bar, testpkg_Baz };',
-    );
   });
 
   test('generates functions for enums', () => {
@@ -263,19 +259,20 @@ function testpkg_Baz(p) {
       { addDeprecationWarnings: true },
     );
 
-    expect(jsFile(result, '.warnings.jsii')).toMatch(`function testpkg_State(p) {
-    if (p == null)
-        return;
-    visitedObjects.add(p);
-    try {
-        if (p === 1)
-            print("testpkg.State#OFF", "Use something else");
-    }
-    finally {
-        visitedObjects.delete(p);
-    }
-}
-`);
+    expect(jsFunction(result, 'testpkg_State', '.warnings.jsii')).toMatchInlineSnapshot(`
+      "function testpkg_State(p) {
+              if (p == null)
+                  return;
+              visitedObjects.add(p);
+              try {
+                  if (p === 1)
+                      print("testpkg.State#OFF", "Use something else");
+              }
+              finally {
+                  visitedObjects.delete(p);
+              }
+          } "
+    `);
   });
 
   test('generates calls for deprecated inherited properties', () => {
@@ -296,47 +293,51 @@ function testpkg_Baz(p) {
       { addDeprecationWarnings: true },
     );
 
-    const warningsFileContent = jsFile(result, '.warnings.jsii');
-
     // For each supertype, its corresponding function should be generated, as usual
-    expect(warningsFileContent).toMatch(`function testpkg_Baz(p) {
-    if (p == null)
-        return;
-    visitedObjects.add(p);
-    try {
-        if ("x" in p)
-            print("testpkg.Baz#x", "message from Baz");
-    }
-    finally {
-        visitedObjects.delete(p);
-    }
-}`);
-    expect(warningsFileContent).toMatch(`function testpkg_Bar(p) {
-    if (p == null)
-        return;
-    visitedObjects.add(p);
-    try {
-        if ("x" in p)
-            print("testpkg.Bar#x", "message from Bar");
-    }
-    finally {
-        visitedObjects.delete(p);
-    }
-}`);
+    expect(jsFunction(result, 'testpkg_Baz', '.warnings.jsii')).toMatchInlineSnapshot(`
+      "function testpkg_Baz(p) {
+              if (p == null)
+                  return;
+              visitedObjects.add(p);
+              try {
+                  if ("x" in p)
+                      print("testpkg.Baz#x", "message from Baz");
+              }
+              finally {
+                  visitedObjects.delete(p);
+              }
+          },"
+    `);
 
-    // But a call for one of the instances of the property should also be generated in the base function
-    expect(warningsFileContent).toMatch(`function testpkg_Foo(p) {
-    if (p == null)
-        return;
-    visitedObjects.add(p);
-    try {
-        if ("x" in p)
-            print("testpkg.Baz#x", "message from Baz");
-    }
-    finally {
-        visitedObjects.delete(p);
-    }
-}`);
+    expect(jsFunction(result, 'testpkg_Bar', '.warnings.jsii')).toMatchInlineSnapshot(`
+      "function testpkg_Bar(p) {
+              if (p == null)
+                  return;
+              visitedObjects.add(p);
+              try {
+                  if ("x" in p)
+                      print("testpkg.Bar#x", "message from Bar");
+              }
+              finally {
+                  visitedObjects.delete(p);
+              }
+          },"
+    `);
+
+    expect(jsFunction(result, 'testpkg_Foo', '.warnings.jsii')).toMatchInlineSnapshot(`
+      "function testpkg_Foo(p) {
+              if (p == null)
+                  return;
+              visitedObjects.add(p);
+              try {
+                  if ("x" in p)
+                      print("testpkg.Baz#x", "message from Baz");
+              }
+              finally {
+                  visitedObjects.delete(p);
+              }
+          } "
+    `);
   });
 
   test('skips properties that are deprecated in one supertype but not the other', () => {
@@ -358,8 +359,7 @@ function testpkg_Baz(p) {
 
     const warningsFileContent = jsFile(result, '.warnings.jsii');
 
-    expect(warningsFileContent).toMatch(`function testpkg_Foo(p) {
-}`);
+    expect(warningsFileContent).not.toContain(`function testpkg_Foo(p)`);
   });
 
   test('generates calls for types with deprecated properties', () => {
@@ -380,21 +380,20 @@ function testpkg_Baz(p) {
       { addDeprecationWarnings: true },
     );
 
-    expect(jsFile(result, '.warnings.jsii')).toMatch(`function testpkg_Foo(p) {
-    if (p == null)
-        return;
-    visitedObjects.add(p);
-    try {
-        if ("bar" in p)
-            print("testpkg.Foo#bar", "kkkkkkkk");
-        if (!visitedObjects.has(p.bar))
-            testpkg_Bar(p.bar);
-    }
-    finally {
-        visitedObjects.delete(p);
-    }
-}
-`);
+    expect(jsFunction(result, 'testpkg_Foo', '.warnings.jsii')).toMatchInlineSnapshot(`
+      "function testpkg_Foo(p) {
+              if (p == null)
+                  return;
+              visitedObjects.add(p);
+              try {
+                  if ("bar" in p)
+                      print("testpkg.Foo#bar", "kkkkkkkk");
+              }
+              finally {
+                  visitedObjects.delete(p);
+              }
+          } "
+    `);
   });
 
   test('generates calls for each property of a deprecated type', () => {
@@ -410,21 +409,22 @@ function testpkg_Baz(p) {
       { addDeprecationWarnings: true },
     );
 
-    expect(jsFile(result, '.warnings.jsii')).toMatch(`function testpkg_Foo(p) {
-    if (p == null)
-        return;
-    visitedObjects.add(p);
-    try {
-        if ("bar" in p)
-            print("testpkg.Foo#bar", "use Bar instead");
-        if ("baz" in p)
-            print("testpkg.Foo#baz", "use Bar instead");
-    }
-    finally {
-        visitedObjects.delete(p);
-    }
-}
-`);
+    expect(jsFunction(result, 'testpkg_Foo', '.warnings.jsii')).toMatchInlineSnapshot(`
+      "function testpkg_Foo(p) {
+              if (p == null)
+                  return;
+              visitedObjects.add(p);
+              try {
+                  if ("bar" in p)
+                      print("testpkg.Foo#bar", "use Bar instead");
+                  if ("baz" in p)
+                      print("testpkg.Foo#baz", "use Bar instead");
+              }
+              finally {
+                  visitedObjects.delete(p);
+              }
+          } "
+    `);
   });
 
   describe('using fixtures', () => {
@@ -439,17 +439,18 @@ function testpkg_Baz(p) {
       lock = undefined;
     }, 120_000);
 
-    test('generates calls for types in other assemblies', async () => {
-      compile(lock!, '@scope/jsii-calc-base-of-base', false);
+    // This code never did what it said it would do. The current code is not
+    // generating calls into validation routines of other assemblies.
+    test.skip('generates calls for types in other assemblies', async () => {
+      compile(lock!, '@scope/jsii-calc-base-of-base', true);
       const calcBaseRoot = compile(lock!, '@scope/jsii-calc-base', true);
       compile(lock!, '@scope/jsii-calc-lib', true, 'deprecated-to-strip.txt');
       const warningsFile = loadWarningsFile(calcBaseRoot);
 
-      // jsii-calc-base was compiled with warnings. So we expect to see handlers for its types in the warnings file
+      // jsii-calc-base was compiled with warnings, and references a type in jsii-calc-base-of-base.
+      // So we expect to see handlers for its types in the warnings file
+      // and we expect it to call into base-of-base.
       expect(warningsFile).toMatch('_scope_jsii_calc_base');
-
-      // jsii-calc-base-of-base was not compiled with warnings. Its types shouldn't be in the warnings file
-      expect(warningsFile).not.toMatch('_scope_jsii_calc_base_of_base');
 
       // Recompiling without deprecation warning to leave the packages in a clean state
       compile(lock!, '@scope/jsii-calc-base', false);
@@ -904,15 +905,33 @@ function jsFile(result: HelperCompilationResult, baseName = 'index'): string {
 }
 
 function jsFunction(result: HelperCompilationResult, functionName: string, baseName = 'index'): string {
-  const lines = jsFile(result, baseName).split(/\n/);
+  const contents = jsFile(result, baseName);
 
-  const startIndex = lines.indexOf(`function ${functionName}(p) {`);
+  const needle = `function ${functionName}(p) {`;
+  const startIndex = contents.indexOf(needle);
   if (startIndex < 0) {
-    throw new Error(`Could not find declaration of ${functionName} in file with base name: ${baseName}`);
+    throw new Error(
+      `Could not find 'function ${functionName}(p) {' in:\n------------------------------\n${contents}\n------------------------------`,
+    );
   }
-  const endIndex = lines.indexOf('}', startIndex);
 
-  return lines.slice(startIndex, endIndex + 1).join('\n');
+  let curlies = 1;
+  let i = startIndex + needle.length;
+  for (; i < contents.length && curlies > 0; i++) {
+    switch (contents[i]) {
+      case '{':
+        curlies += 1;
+        break;
+      case '}':
+        curlies -= 1;
+        break;
+      default:
+        break;
+    }
+  }
+  const endIndex = i;
+
+  return contents.slice(startIndex, endIndex + 1);
 }
 
 function createVmContext(compilation: HelperCompilationResult) {

test/__snapshots__/integration.test.ts.snap
@@ -67,7 +67,7 @@ exports[`integration test: @scope/jsii-calc-base 1`] = `
     "@scope/jsii-calc-base.deep": {
       "locationInModule": {
         "filename": "lib/index.ts",
-        "line": 39,
+        "line": 43,
       },
       "symbolId": "lib/deep/index:",
     },
@@ -141,7 +141,7 @@ exports[`integration test: @scope/jsii-calc-base 1`] = `
       "kind": "interface",
       "locationInModule": {
         "filename": "lib/index.ts",
-        "line": 22,
+        "line": 26,
       },
       "name": "BaseProps",
       "properties": [
@@ -150,7 +150,7 @@ exports[`integration test: @scope/jsii-calc-base 1`] = `
           "immutable": true,
           "locationInModule": {
             "filename": "lib/index.ts",
-            "line": 23,
+            "line": 27,
           },
           "name": "bar",
           "type": {
@@ -169,14 +169,14 @@ exports[`integration test: @scope/jsii-calc-base 1`] = `
       "kind": "interface",
       "locationInModule": {
         "filename": "lib/index.ts",
-        "line": 26,
+        "line": 30,
       },
       "methods": [
         {
           "abstract": true,
           "locationInModule": {
             "filename": "lib/index.ts",
-            "line": 27,
+            "line": 31,
           },
           "name": "bar",
         },
@@ -194,13 +194,13 @@ exports[`integration test: @scope/jsii-calc-base 1`] = `
       "kind": "class",
       "locationInModule": {
         "filename": "lib/index.ts",
-        "line": 33,
+        "line": 37,
       },
       "methods": [
         {
           "locationInModule": {
             "filename": "lib/index.ts",
-            "line": 34,
+            "line": 38,
           },
           "name": "consume",
           "parameters": [
@@ -218,6 +218,32 @@ exports[`integration test: @scope/jsii-calc-base 1`] = `
       ],
       "name": "StaticConsumer",
       "symbolId": "lib/index:StaticConsumer",
+    },
+    "@scope/jsii-calc-base.TypeToContainVeryBaseProps": {
+      "assembly": "@scope/jsii-calc-base",
+      "datatype": true,
+      "fqn": "@scope/jsii-calc-base.TypeToContainVeryBaseProps",
+      "kind": "interface",
+      "locationInModule": {
+        "filename": "lib/index.ts",
+        "line": 22,
+      },
+      "name": "TypeToContainVeryBaseProps",
+      "properties": [
+        {
+          "abstract": true,
+          "immutable": true,
+          "locationInModule": {
+            "filename": "lib/index.ts",
+            "line": 23,
+          },
+          "name": "veryBaseProps",
+          "type": {
+            "fqn": "@scope/jsii-calc-base-of-base.VeryBaseProps",
+          },
+        },
+      ],
+      "symbolId": "lib/index:TypeToContainVeryBaseProps",
     },
     "@scope/jsii-calc-base.deep.BarrelImportClass": {
       "assembly": "@scope/jsii-calc-base",

commit fdb95175748cca2299cc28e0fd6e217ce75ec451

fix: transitive deprecation warnings are broken
In #2620, we changed how the deprecation warnings file is generated.

We then made the observation that everything was in place to generate
validation calls to other assemblies, but that they weren't used.

That wasn't entirely true:

- `TypeFromOtherAssembly`: not validated.
- `TypeFromOtherAssembly[]`: validated.
- `TypeFromOtherAssembly | string`: validated.

It's unclear if this was intentional or not, but it is the current
behavior.

And the refactoring we did in the previous PR broke the code get gets
emitted for the cases in which we *do* generate the cross-assembly call.

Fix the bug, uncomment the test, and document the current behavior.

fixtures/@scope/jsii-calc-base/lib/index.ts
@@ -20,7 +20,7 @@ export abstract class Base {
 }
 
 export interface TypeToContainVeryBaseProps {
-  readonly veryBaseProps: VeryBaseProps;
+  readonly veryBaseProps: VeryBaseProps[];
 }
 
 export interface BaseProps extends VeryBaseProps {

src/transforms/deprecation-warnings.ts
@@ -128,10 +128,6 @@ export class DeprecationWarningsInjector {
       return this.shouldRenderValidatorCache[type.fqn];
     }
 
-    if (type.fqn === '@scope/jsii-calc-base.TypeToContainVeryBaseProps') {
-      debugger;
-    }
-
     if (this.validatorCacheSeenSet.has(type.fqn)) {
       // To be safe we need to say this is true.
       return true;
@@ -757,7 +753,7 @@ function importedFunctionName(typeName: string, assembly: Assembly, projectInfo:
   if (type) {
     return moduleName !== assembly.name
       ? `require("${moduleName}/${WARNINGSCODE_FILE_NAME}").${fnName(type.fqn)}`
-      : fnName(type.fqn);
+      : `module.exports.${fnName(type.fqn)}`;
   }
   return undefined;
 }
@@ -839,7 +835,7 @@ function createTypeHandlerCall(
         ),
         ts.factory.createExpressionStatement(
           ts.factory.createCallExpression(
-            ts.factory.createPropertyAccessExpression(ts.factory.createIdentifier('module.exports'), functionName),
+            ts.factory.createIdentifier(functionName),
             undefined,
             [ts.factory.createIdentifier(parameter)],
           ),

test/deprecation-warnings.test.ts
@@ -440,8 +440,8 @@ class DeprecationError extends Error {
     }, 120_000);
 
     // This code never did what it said it would do. The current code is not
-    // generating calls into validation routines of other assemblies.
-    test.skip('generates calls for types in other assemblies', async () => {
+    // generating calls into validation routines of other assemblies
+    test('generates calls for types in other assemblies', async () => {
       compile(lock!, '@scope/jsii-calc-base-of-base', true);
       const calcBaseRoot = compile(lock!, '@scope/jsii-calc-base', true);
       compile(lock!, '@scope/jsii-calc-lib', true, 'deprecated-to-strip.txt');
@@ -450,7 +450,22 @@ class DeprecationError extends Error {
       // jsii-calc-base was compiled with warnings, and references a type in jsii-calc-base-of-base.
       // So we expect to see handlers for its types in the warnings file
       // and we expect it to call into base-of-base.
-      expect(warningsFile).toMatch('_scope_jsii_calc_base');
+      expect(extractFunction(warningsFile, '_scope_jsii_calc_base_TypeToContainVeryBaseProps')).toMatchInlineSnapshot(`
+        "function _scope_jsii_calc_base_TypeToContainVeryBaseProps(p) {
+                if (p == null)
+                    return;
+                visitedObjects.add(p);
+                try {
+                    if (p.veryBaseProps != null)
+                        for (const o of p.veryBaseProps)
+                            if (!visitedObjects.has(o))
+                                require("@scope/jsii-calc-base-of-base/.warnings.jsii.js")._scope_jsii_calc_base_of_base_VeryBaseProps(o);
+                }
+                finally {
+                    visitedObjects.delete(p);
+                }
+            } "
+      `);
 
       // Recompiling without deprecation warning to leave the packages in a clean state
       compile(lock!, '@scope/jsii-calc-base', false);
@@ -906,7 +921,10 @@ function jsFile(result: HelperCompilationResult, baseName = 'index'): string {
 
 function jsFunction(result: HelperCompilationResult, functionName: string, baseName = 'index'): string {
   const contents = jsFile(result, baseName);
+  return extractFunction(contents, functionName);
+}
 
+function extractFunction(contents: string, functionName: string): string {
   const needle = `function ${functionName}(p) {`;
   const startIndex = contents.indexOf(needle);
   if (startIndex < 0) {