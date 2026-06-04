fc7303fb1039bbcd68900fb90a5504f75f9a4626
Omar Qureshi <omar@omarqureshi.net>
2026-06-04 14:33:40 +0100

    Add support for Ruby

--- src/assembler.ts
+++ src/assembler.ts
@@ -2811,6 +2811,7 @@ export class Assembler implements Emitter {
     const javaPackages: Record<string, string[]> = {};
     const pythonModules: Record<string, string[]> = {};
     const goPackages: Record<string, string[]> = {};
+    const rubyModules: Record<string, string[]> = {};
 
     for (const submodule of this._submodules.values()) {
       const targets = submodule.targets as AssemblyTargets | undefined;
@@ -2827,12 +2828,16 @@ export class Assembler implements Emitter {
       if (targets?.go?.packageName) {
         accumList(goPackages, targets.go.packageName, submodule.fqn);
       }
+      if (targets?.ruby?.module) {
+        accumList(rubyModules, targets.ruby.module, submodule.fqn);
+      }
     }
 
     maybeError('dotnet', dotNetnamespaces);
     maybeError('java', javaPackages);
     maybeError('python', pythonModules);
     maybeError('go', goPackages);
+    maybeError('ruby', rubyModules);
 
     function accumList(set: Record<string, string[]>, key: string, value: string) {
       if (!set[key]) {
