b0e9b7645d051b52f65ad8f43075b03be73e0b09
dgandhi62 <gandhidhyan@gmail.com>
2026-07-02 13:36:29 -0400

    chore: moved pr branch name through env variable

--- projenrc/build-workflow.ts
+++ projenrc/build-workflow.ts
@@ -168,10 +168,13 @@ export class BuildWorkflow {
           },
           {
             name: 'Push changes',
+            env: {
+              HEAD_REF: '${{ github.event.pull_request.head.ref }}',
+            },
             run: [
               'git add .',
               'git commit -s -m "chore: self-mutation"',
-              'git push origin HEAD:${{ github.event.pull_request.head.ref }}',
+              'git push origin "HEAD:$HEAD_REF"',
             ].join('\n'),
           },
         ],
