9f048b60ee0311756b91429432456fb41da7b75f
Dhyan Gandhi <gandhidhyan@gmail.com>
2026-07-02 13:55:14 -0400

    chore: route PR head ref through env var instead of inline (#2707)
    Referencing event context inline in a run: block substitutes the raw
    value into the script text, where the shell then interprets it as code.
    Routing values through env: passes them to the script as data instead,
    so their contents are never parsed as part of the command.
    
    ---
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the [Apache 2.0 license].
    
    [Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>
    (cherry picked from commit bcf1bd647bf80eb03fbf9c99690099bb2c1c5b4e)

--- projenrc/build-workflow.ts
+++ projenrc/build-workflow.ts
@@ -153,11 +153,12 @@ export class BuildWorkflow {
           },
           {
             name: 'Push changes',
-            run: [
-              'git add .',
-              'git commit -s -m "chore: self-mutation"',
-              'git push origin HEAD:${{ github.event.pull_request.head.ref }}',
-            ].join('\n'),
+            env: {
+              HEAD_REF: '${{ github.event.pull_request.head.ref }}',
+            },
+            run: ['git add .', 'git commit -s -m "chore: self-mutation"', 'git push origin "HEAD:$HEAD_REF"'].join(
+              '\n',
+            ),
           },
         ],
       },
